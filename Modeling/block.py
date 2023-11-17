1124132070b6f2a1e5baa23b3c0e85102a77026f
Mfofana <mamoutou1992fofana@gmail.com>
2023-11-17 13:04:26 +0100

    decode block added

--- Modeling/block.py
+++ Modeling/block.py
@@ -1,19 +1,35 @@
-class Block(nn.module):
+"""
+Created on Thu Nov  16 17:20:39 2023
 
+@author: Mamoutou Fofana
+"""
+
+import torch.nn as nn
+
+
+from multi_head_attention import MultiHeadAttention
+from feed_forward import FeedForward
+
+
+class Block(nn.Module):
+  """
+  Transformer block : communication followed by computation
+  """
   def __init__(self, num_embed, num_head):
     super().__init__()
     head_size = num_embed // num_head
-    self.self_attention = MultiheadAttention(num_head, head_size)
+    self.self_attention = MultiHeadAttention(num_head, head_size)
     self.feed_forward = FeedForward(num_embed)
-    self.linear_1 = nn.LayerNorm(num_embed)
-    self.linear_2 = nn.LayerNorm(num_embed)
+    self.linear1 = nn.LayerNorm(num_embed)
+    self.linear2 = nn.LayerNorm(num_embed)
 
 
-def forward(self, x):
+  def forward(self, x):
 
-  y = self.self_attention(x)
-  x = self.linear_1(x + y)
-  y = self.feed_forward(x + y)
-  x = self.linear_2(x + y)
+    y = self.self_attention(x)
+    print(y)
+    x = self.linear1(x + y)
+    y = self.feed_forward(x)
+    x = self.linear2(x + y)
 
-  return x
+    return x
